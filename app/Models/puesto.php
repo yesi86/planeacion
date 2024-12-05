1ce554bb9452eb1a3bb66b04bda60c287924c5df
ADavidOli <angeldoliv85@gmail.com>
2024-12-04 18:24:43 -0600

    creacion de la tabla puesto

--- app/Models/puesto.php
+++ app/Models/puesto.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class puesto extends Model
 {
-    protected $table = 'puesto';
+    use HasFactory;
+
+    protected $fillable = ['name'];
 }
