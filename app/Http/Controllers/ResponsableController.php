a26cc8b2dcf72dfb11fc7b9acf74eff531e7776d
ADavidOli <angeldoliv85@gmail.com>
2024-12-16 03:40:17 -0600

    prueba de la base de datos con jerarquia y relacion, usar seeder

--- app/Http/Controllers/ResponsableController.php
+++ app/Http/Controllers/ResponsableController.php
@@ -20,7 +20,7 @@ class ResponsableController extends Controller
             return redirect()->route('dashboard')
                 ->with('alert', 'No tienes permisos para acceder a esta página');
         }
-        $responsables = User::role('Responsable')->paginate(10);
+        $responsables = User::role('Responsable De Area')->paginate(10);
 
         return view('responsable.index', compact('responsables'));
     }
